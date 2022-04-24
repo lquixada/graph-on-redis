cc761b8830b0d9ce101aa6f1f8c044faff23b1e4
Leonardo Quixada <lquixada@gmail.com>
2022-04-24 14:54:13 -0400

    chore: added npm tasks to flushdb and graph.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Node = require('./model/node');
 
 const node1 = new Node(1);
 
-node1.getNeighbors().then(data => {
-  console.log(data);
-}).catch(err => console.error(err))
+node1.getNeighbors()
+  .then(data => console.log(data))
+  .then(process.exit)
+  .catch(err => console.error(err))
